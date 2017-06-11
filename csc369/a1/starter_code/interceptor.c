0e6d35c955ca1958711f282db180d180456eaf8d
Kevin Kim <kevincolour@gmail.com>
2017-06-10 20:32:12 -0400

    .

--- csc369/a1/starter_code/interceptor.c
+++ csc369/a1/starter_code/interceptor.c
@@ -431,7 +431,7 @@ static int init_function(void) {
 	void (*my_exit_group_ptr)(int) = my_exit_group;
 	set_addr_rw((unsigned long)__NR_exit_group);
 
-	sys_call_table[__NR_exit_group] = my_exit_group_ptr;
+//	sys_call_table[__NR_exit_group] = my_exit_group_ptr;
 	set_addr_ro((unsigned long)__NR_exit_group);
 //	sys_call_table[MY_CUSTOM_SYSCALL] = my_syscall;
 //	set_addr_ro((unsigned long)sys_call_table);
