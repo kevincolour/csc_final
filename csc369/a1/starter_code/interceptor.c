353cc590a340a3ca628c1c6a3bfe8acc4dd8f967
Kevin Kim <kevincolour@gmail.com>
2017-06-12 12:25:48 -0400

    .

--- csc369/a1/starter_code/interceptor.c
+++ csc369/a1/starter_code/interceptor.c
@@ -427,10 +427,11 @@ static int init_function(void) {
 
 	orig_custom_syscall = sys_call_table[MY_CUSTOM_SYSCALL];
 	orig_exit_group = sys_call_table[__NR_exit_group];
-	printk(KERN_ALERT "hello");
+	printk(KERN_ALERT "hello /n");
+	void (*my_exit_ptr)(int) = my_exit_group;
 	set_addr_rw((unsigned long)sys_call_table);
-	sys_call_table[__NR_exit_group] = & my_exit_group;
-	sys_call_table[MY_CUSTOM_SYSCALL] = my_syscall;
+	sys_call_table[__NR_exit_group] = my_exit_ptr;
+	sys_call_table[MY_CUSTOM_SYSCALL] = &my_syscall;
 	set_addr_ro((unsigned long)sys_call_table);
 
 
@@ -460,14 +461,14 @@ static int init_function(void) {
  */
 static void exit_function(void)
 {        
-	/*
+	
 	spin_lock(&calltable_lock);
 	set_addr_rw((unsigned long)sys_call_table);
-	sys_call_table[MY_CUSTOM_SYSCALL] = orig_custom_syscall;
-	sys_call_table[__NR_exit_group] = orig_exit_group;
+	sys_call_table[MY_CUSTOM_SYSCALL] = &orig_custom_syscall;
+	sys_call_table[__NR_exit_group] = &orig_exit_group;
 	set_addr_ro((unsigned long)sys_call_table);
 	spin_unlock(&calltable_lock);
-	*/
+	
 }
 
 module_init(init_function);
