b3c3d5e2302695eb01a9118826db1d53a73de0b6
Kevin Kim <kevincolour@gmail.com>
2017-06-12 13:12:04 -0400

    .

--- csc369/a1/starter_code/interceptor.c
+++ csc369/a1/starter_code/interceptor.c
@@ -336,7 +336,7 @@ asmlinkage long interceptor(struct pt_regs reg) {
  */
 asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 
-
+	/*
 	int req_proc = current_uid();
 	if (syscall < 0 || syscall > NR_syscalls || syscall == MY_CUSTOM_SYSCALL){
 		return -EINVAL;
@@ -393,12 +393,11 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 		set_addr_ro((unsigned long)sys_call_table);
 		spin_unlock(&calltable_lock);
 	}
-
+	*/
 	return 0;
 }
 
-
-
+	
 long (*orig_custom_syscall)(void);
 
 
