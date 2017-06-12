52a97d7c8232d7e960e4cb64ca5f8aadd165c2c6
Kevin Kim <kevincolour@gmail.com>
2017-06-12 18:17:50 -0400

    .

--- csc369/a1/starter_code/interceptor.c
+++ csc369/a1/starter_code/interceptor.c
@@ -501,6 +501,7 @@ static void exit_function(void)
 	sys_call_table[MY_CUSTOM_SYSCALL] = orig_custom_syscall;
 	sys_call_table[__NR_exit_group] = orig_exit_group;
 	// destroy all lists of monitored pids.
+	int i;
 	for (i = 0; i < NR_syscalls; i++){ 
 		destroy_list(i);
 	}	
